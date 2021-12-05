3f4e80e76b2a0032f7f974788f14a0c93797be30
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2021-12-05 12:37:23 +0100

    Added day 5 solutions

---
+++ day5/puzzle2.js
@@ -0,0 +1,77 @@
+(() => {
+    function* range(a, b, inclusiveEnd = true) {
+        if (a === b) {
+            if (inclusiveEnd) yield a;
+            return;
+        }
+        const increment = Math.sign(b - a);
+        const pred =
+            increment > 0
+                ? (i) => i < b || (inclusiveEnd && i <= b)
+                : (i) => i > b || (inclusiveEnd && i >= b);
+        for (let i = a; pred(i); i += increment) {
+            yield i;
+        }
+    }
+    function* drawLine({ start: { x: startX, y: startY }, end: { x: endX, y: endY } }) {
+        const diffX = endX - startX;
+        const diffY = endY - startY;
+        if (startX === endX) {
+            // vertical
+            for (const p of range(startY, endY)) {
+                yield [startX, p];
+            }
+        } else if (startY === endY) {
+            // horizontal
+            for (const p of range(startX, endX)) {
+                yield [p, startY];
+            }
+        } else if (Math.abs(diffX) === Math.abs(diffY)) {
+            // diagonal
+            const compute = {
+                [1]: (p) => [startX + p, startY + p],
+                [-1]: (p) => [startX + p, startY - p],
+            };
+            const coord = compute[Math.sign(diffX) * Math.sign(diffY)];
+            for (const p of range(0, diffX)) {
+                yield coord(p);
+            }
+        }
+    }
+    const data = document.querySelector("pre").textContent.trim().split("\n");
+    const lines = data.map((l) => {
+        const [start, end] = l.split("->").map((p) => {
+            const [x, y] = p
+                .trim()
+                .split(",")
+                .map((v) => parseInt(v));
+            return { x, y };
+        });
+        return { start, end };
+    });
+    console.log(lines);
+    const { maxX, maxY } = lines
+        .flatMap(({ start, end }) => [start, end])
+        .reduce(
+            ({ maxX, maxY }, { x, y }) => ({
+                maxX: x > maxX ? x : maxX,
+                maxY: y > maxY ? y : maxY,
+            }),
+            { maxX: 0, maxY: 0 },
+        );
+    console.log({ maxX, maxY });
+    const grid = Array(maxX + 1)
+        .fill(0)
+        .map(() => Array(maxY + 1).fill(0));
+    for (const line of lines) {
+        for (const [x, y] of drawLine(line)) {
+            grid[x][y] = grid[x][y] + 1;
+        }
+    }
+    console.log(grid);
+    const overTwo = grid.reduce(
+        (acc, row) => acc + row.map((val) => +(val >= 2)).reduce((l, r) => l + r),
+        0,
+    );
+    console.log(overTwo);
+})();
