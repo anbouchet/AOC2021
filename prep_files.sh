2fe5197c38b6eb9edb89e155b3eb76cfab960467
Ancelin Bouchet <ancelin.bouchet@codingame.com>
2021-12-07 09:49:36 +0100

    Added small utility script to start working on solutions

---
+++ prep_files.sh
@@ -0,0 +1,11 @@
+if [ "$#" -lt 2 ]; then
+    echo "Usage: ./prep_files.sh <day_number> <file_ext>"
+fi
+
+day="$1"
+ext="$2"
+
+mkdir "day$day"
+touch "day$day/puzzle1.$ext" "day$day/puzzle2.$ext"
+
+echo "folder and base files for day $day have been created with the .$ext extension"
